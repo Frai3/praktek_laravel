4be836e8a4c07220adf62942da05c9c1e55a92db
Frai3 <farhan.raihan@student.umn.ac.id>
2022-11-28 15:14:21 +0700

    praktek_laravel

--- app/Http/Controllers/ToyotaController.php
+++ app/Http/Controllers/ToyotaController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Toyota;
 use Illuminate\Support\Facades\File;
-use App\Http\Controllers\Validator;
+// use App\Http\Controllers\Validator;
 
 class ToyotaController extends Controller
 {
